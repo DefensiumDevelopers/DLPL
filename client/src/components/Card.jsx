3339c79c9db1b367ad8757c1559fd36b880fb802
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-06-29 14:31:23 +0530

    increase width of card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -19,14 +19,14 @@ const Card = () => {
     return (
         <>
             {cards.map((item, index) => (
-                <div key={index} className="w-[300px] h-[420px] bg-transparent cursor-pointer group perspective rounded-lg">
+                <div key={index} className="w-[300px] h-[420px] sm:w-[330px] bg-transparent cursor-pointer group perspective rounded-lg">
                     <div className="relative preserve-3d group-hover:my-rotate-x-180 w-full h-full duration-500 rounded-lg shadow-xl">
                         <div className='absolute backface-hidden w-full h-full shadow-xl rounded-lg'>
                             <img src="https://media.istockphoto.com/id/1473719070/photo/3d-internet-secuirty-badge.jpg?s=2048x2048&w=is&k=20&c=kcZyrooZi-T8kdM9LeoFbN0v2zykniC7XVxNG_TYVqk=" alt="" className='w-full h-full object-cover rounded-lg' />
                         </div>
                         <div className='absolute backface-hidden my-rotate-x-180 w-full h-full bg-white rounded-lg'>
                             <div className='text-center flex flex-col items-center justify-around h-full'>
-                                <h1 className='text-4xl font-bold text-blue-600'>{item.title}</h1>
+                                <h1 className='text-4xl font-bold text-blue-600 px-16'>{item.title}</h1>
                                 <p className='p-5 text-gray-700'>
                                     {item.desc}
                                 </p>
